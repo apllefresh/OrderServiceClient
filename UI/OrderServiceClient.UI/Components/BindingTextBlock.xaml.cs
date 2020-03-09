e46e02c45cec64a411da125c1fc92f46af5b3038
Константин Алексеев <arrogant93@mail.ru>
2020-03-09 04:25:27 +0300

    added simple UI component

---
+++ UI/OrderServiceClient.UI/Components/BindingTextBlock.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace OrderServiceClient.UI.Components
+{
+    public partial class BindingTextBlock : UserControl
+    {
+        public string Label
+        {
+            get { return (string)GetValue(LabelProperty); }
+            set { SetValue(LabelProperty, value); }
+        }
+
+        public static readonly DependencyProperty LabelProperty = DependencyProperty.Register("Label", typeof(string), typeof(BindingTextBlock), new FrameworkPropertyMetadata(""));
+
+        public string Value
+        {
+            get { return (string)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(string), typeof(BindingTextBlock), new FrameworkPropertyMetadata(""));
+
+        public BindingTextBlock()
+        {
+            InitializeComponent();
+        }
+    }
+}
